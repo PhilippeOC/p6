08b7b424c480b3411246dbfc4b9e2903ce070714
philippe <philoufilou2@gmail.com>
2021-04-29 10:38:40 +0200

    gestion des affichqges, css et compabilité navigateur

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 
-
-
-main();
-
 async function main()
 {
+
     const API_URL = "http://localhost:8000/api/v1/titles/";
-    const nbMoviesLoad = 7;    //nombre de films téléchargés par catégorie
+    const nbMoviesToLoad = 7;    //nombre de films à télécharger par catégorie
+    let nbImg = 4; //nombre d'images affichées simultanément par catégorie
     const cat1 = 'action';
-    const cat2 = 'animation';
+    const cat2 = 'biography';
 
     let moviesById={};  //tableau dynamique contenant tous les films clé: id du film / valeur: données du film
-    let nbImg = 4; //nombre d'image afficher par catégorie
-    //let indexStart;
+   
 
     //le meilleur film
     let idBestMovie = await findIdBestMovie(API_URL+'?sort_by=-imdb_score&page=');
@@ -21,23 +18,22 @@ async function main()
     displayDatasBestMovie(dataMovie);
 
     //les autres meilleurs films
-    let betsOfMovies = await getBestMovies(API_URL, idBestMovie,nbMoviesLoad, 'allCat');
-    //displayBestMoviesImages(betsOfMovies, "other_best_movie");
+    let betsOfMovies = await getBestMovies(API_URL, idBestMovie,nbMoviesToLoad, 'allCat');
     moviesById = findMovieById(betsOfMovies, moviesById);
-    //idImageArray(betsOfMovies);
-    displayAllImges(nbImg, betsOfMovies, "other_best_movie");
+    displayCategoryName("top_rated_movies",'films les mieux notés');
+    displayAllImges(nbImg, betsOfMovies, "other_best_movie",nbMoviesToLoad);
     
     //meilleurs films de la catégorie 1 
-    let betsOfMoviesCat1 = await getBestMovies(API_URL, idBestMovie,nbMoviesLoad, cat1);
-    //displayBestMoviesImages(betsOfMoviesCat1, "cat_1");
+    let betsOfMoviesCat1 = await getBestMovies(API_URL, idBestMovie,nbMoviesToLoad, cat1);
     moviesById = findMovieById(betsOfMoviesCat1, moviesById);
-    displayAllImges(nbImg, betsOfMoviesCat1, "cat_1");
-    
+    displayCategoryName("top_rated_movies_cat_1",cat1);
+    displayAllImges(nbImg, betsOfMoviesCat1, "cat_1",nbMoviesToLoad);
+
     //meilleurs films de la catégorie 2
-    let betsOfMoviesCat2 = await getBestMovies(API_URL, idBestMovie,nbMoviesLoad, cat2);
-    //displayBestMoviesImages(betsOfMoviesCat2, "cat_2");
+    let betsOfMoviesCat2 = await getBestMovies(API_URL, idBestMovie,nbMoviesToLoad, cat2);
     moviesById = findMovieById(betsOfMoviesCat2, moviesById);
-    displayAllImges(nbImg, betsOfMoviesCat2, "cat_2");
+    displayCategoryName("top_rated_movies_cat_2",cat2);
+    displayAllImges(nbImg, betsOfMoviesCat2, "cat_2",nbMoviesToLoad);
    
 
     clicOnImage(moviesById);
@@ -45,7 +41,12 @@ async function main()
 };
 
 
+function displayCategoryName(container,category)
+{
+    document.getElementById(container).textContent= category.charAt(0).toUpperCase() 
+                                                  + category.substring(1).toLowerCase();
 
+}
 
 function createDivImg(nbImg,container)
 {
@@ -53,9 +54,9 @@ function createDivImg(nbImg,container)
     //for (i in nbDiv)
     {
         let divImg = document.createElement("div");
-    
         divImg.id = 'div_'+container+'_' +i.toString();
-        divImg.setAttribute("class", "divImage"); 
+        divImg.setAttribute("class", "divImage divImageCat"); 
+
         document.getElementById(container).appendChild(divImg);
         let newImg= document.createElement("img");
         newImg.setAttribute("class", "image"); 
@@ -66,8 +67,7 @@ function createDivImg(nbImg,container)
 
 
 function displayImg(nbImg, data, indexStart, container)
-{   
-             
+{                
     for(i=indexStart; i<(nbImg+indexStart); i++)
     {
         let newImage = document.querySelector('#div_'+container+'_' +(i-indexStart).toString()+">img")
@@ -80,20 +80,15 @@ function displayImg(nbImg, data, indexStart, container)
 };
 
 
-function displayAllImges(nbImg, data, container)
+function displayAllImges(nbImg, data, container, nbMoviesLoad)
 // gestion de l'affichage des images des films via le carrousel
 {
     const indexStart = 0;
     createDivImg(nbImg,container);
-    createCarouselArrow(container);
+    createCarouselArrow(nbImg, container);
     displayImg(nbImg, data, indexStart,container);
-    carousel(nbImg,data,indexStart,container);
+    carousel(nbImg,data,indexStart,container,nbMoviesLoad);
 }
-
-
-
-
-
 
 function findMovieById(data, moviesById)
 //retourne le tableau {clé: id du film / valeur: données du film}
@@ -105,29 +100,21 @@ function findMovieById(data, moviesById)
     return moviesById;
 };
 
-/*
-function displayBestMoviesImages(data, container)
-    {
-        for (i in data)
-        {
-            displayDatas(data[i],"image_url",container);
-        };
-    };*/
-
-
 function displayDatasBestMovie(data)
 {
     let newImg = document.createElement("img");
     newImg.src = data.image_url;
     newImg.id = data.id;
-    newImg.setAttribute("class", "image");         
-    document.getElementById("title_best_movie").innerHTML= `<h2>${data.title}</h2>`;
-    document.getElementById("description_best_movie").innerHTML= `<p>${data.description}</p>`;
+    newImg.setAttribute("class", "image imageBestMovie");         
+    document.getElementById("title_best_movie").innerHTML= `${data.title}`;
+    //document.getElementById("description_best_movie").innerHTML= `<p>${data.description} Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam magna tellus, viverra sit amet elit vel, porta mollis lacus. Nam congue, lacus eget auctor imperdiet, enim neque porttitor ligula, vitae fermentum massa dolor at turpis. Sed fermentum metus eget eros semper sollicitudin. Nunc ornare id magna commodo feugiat. Phasellus ligula arcu, sodales ac dolor eget, eleifend tincidunt lorem. Pellentesque dictum vestibulum nunc id maximus. Nam hendrerit arcu sed ipsum rhoncus placerat. Donec augue lectus, lobortis ut laoreet eu, congue eget ligula. Sed eleifend ut felis eu condimentum. Nam velit lectus, vulputate at dignissim eget, porta vel turpis. Aenean vehicula lectus vitae dolor feugiat, non finibus magna eleifend. Mauris diam tortor, sollicitudin sit amet elementum in, malesuada a magna. Sed congue sit amet dolor sed ultricies.</p>`;
+    
+    document.getElementById("description_best_movie").innerHTML= `<p>${data.description} </p>`;
     document.getElementById("best_movie").appendChild(newImg);
 };
 
 
-async function getBestMovies(urlApi, idBestMovie, numberMax, category)
+async function getBestMovies(urlApi, idBestMovie, nbMoviesLoad, category)
 {
     let url;
     let datasBestsMovies = [];
@@ -140,14 +127,14 @@ async function getBestMovies(urlApi, idBestMovie, numberMax, category)
         url = "http://localhost:8000/api/v1/titles?genre=" + category + '&sort_by=-imdb_score&page='
     };
 
-    let idArray = await getIdMovies(url, numberMax+1);
+    let idArray = await getIdMovies(url, nbMoviesLoad+1);
     for(i in idArray)
     {
         if (idArray[i] != idBestMovie)
         {
             let datasMovie = await getDatasMovieById(urlApi,idArray[i]);
             datasBestsMovies.push(datasMovie);
-            if (datasBestsMovies.length == numberMax)
+            if (datasBestsMovies.length == nbMoviesLoad)
             {break;}
         };
     
@@ -267,13 +254,24 @@ function clickOnOpenButton(movieById,idBestMovie)
 
 function clicOnImage(movieById)
 {
-    let imgCliked = document.querySelectorAll('.image');   
+    let imgCliked = document.querySelectorAll('.image');
     for (i=0; i< imgCliked.length; i++)
     {
+
+     
     imgCliked[i].addEventListener('click',  function(event) 
         {           
             document.getElementById("box_modal").style.display = "block";
-            displayDatasInModal(movieById[event.path[0].id.toString()]);   
+            console.log(navigator.userAgent.indexOf("Firefox"));
+            if (navigator.userAgent.indexOf("Firefox")>=1)
+                {
+                    displayDatasInModal(movieById[event.explicitOriginalTarget.id.toString()]);
+                }
+            else
+                {
+                    displayDatasInModal(movieById[event.path[0].id.toString()]);
+                }
+               
             clickOnCloseButton();
         })
     }
@@ -294,7 +292,7 @@ function displayDatasInModal(data)
 {
     document.getElementById("datas_modal_text").innerHTML= 
         `<h3> ${data.title}</h3>
-        <p><span class='modalItems'>Description:</span> ${data.description}  <br> 
+        <p><span class='modalItems'>Description:</span> ${data.description} <br> 
         <span class='modalItems'>Gender(s):</span> ${data.gender} <br>
         <span class='modalItems'>Publication:</span> ${data.date_published} <br> 
         <span class='modalItems'>Rated:</span> ${data.rated} <br>
@@ -304,34 +302,34 @@ function displayDatasInModal(data)
         <span class='modalItems'>Durée:</span> ${data.duration} <br>
         <span class='modalItems'>Pays:</span> ${data.countries} <br>
         </p> `; 
-    //let modalImg = document.createElement("img");
+    
     let modalImg = document.querySelector("#modal_img");
     modalImg.src = data.image_url;
-   //console.log(data.image_url);
-    //console.log(modalImg);
-    //modalImg.id = data.id + '_modal';
-    //modalImg.setAttribute("class", "imageModal"); 
-    //document.getElementById("img").appendChild(modalImg);
-    //document.getElementById("datas_modal_img").HTMLImageElement.modalImg;
+  
 };
 
 
 
-function createCarouselArrow(container)
+function createCarouselArrow(nbImg,container)
 {
     let divArrowLeft = document.createElement("div");
     divArrowLeft.id = 'arrowLeft_'+container;
-    divArrowLeft.setAttribute("class", "arrowLeft unselectable"); 
+    divArrowLeft.setAttribute("class", "arrowLeft unselectable divImageCat"); 
     document.getElementById(container).appendChild(divArrowLeft).textContent= "<";
 
     let divArrowRight = document.createElement("div");
     divArrowRight.id = 'arrowRight_'+container;
-    divArrowRight.setAttribute("class", "arrowRight unselectable"); 
+    divArrowRight.setAttribute("class", "arrowRight unselectable divImageCat"); 
     document.getElementById(container).appendChild(divArrowRight).textContent= ">";
+
+    let arrowLeftPlace = document.querySelector("#" + container + " >.divImageCat:nth-child("+ (nbImg+1).toString() +")");
+    arrowLeftPlace.setAttribute("style","order:-1");
+    let arrowRightPlace = document.querySelector("#" + container + " >.divImageCat:last-child");
+    arrowRightPlace.setAttribute("style","order:" + (nbImg+2).toString());
 
 };
 
-function carousel(nbImg,data ,indexStart,container)
+function carousel(nbImg,data ,indexStart,container,nbMoviesLoad)
 {
     let arrowLeftCliked = document.querySelector('#arrowLeft_'+container);   
     
@@ -345,15 +343,16 @@ function carousel(nbImg,data ,indexStart,container)
             displayImg(nbImg, data, indexStart,container);   
         });
 
-    let arrowRightCliked = document.querySelector('#arrowRight_'+container);   
+    let arrowRightCliked = document.querySelector('#arrowRight_'+container);  
+    
     
         arrowRightCliked.addEventListener('click',  function() 
         {           
             
             indexStart = indexStart+1;
-            if (indexStart>(nbImg-1))
+            if (indexStart>(nbMoviesLoad-nbImg))
             {
-                indexStart = nbImg-1;
+                indexStart = indexStart-1;
             } 
             displayImg(nbImg, data, indexStart,container);            
         })
@@ -363,6 +362,8 @@ function carousel(nbImg,data ,indexStart,container)
         
 };
 
+
+main();
 
  
     
